1662ee1f963506f372ccd39fc5edded98aa65026
Steffan <sachtmann121@gmail.com>
2020-02-08 00:05:30 -0800

    Finished up login form, revertted db insertion stuff in register form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
+    // Deconstruct form data
+    const {email, password} = formData
+
+    // Generic onChange handler for our form
+    //uses the 'name' field of the input to determine which field to update in our state
+    const changeHandler = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+    }
+
+    // Remember, needs to be async b/c we need to do DB stuff
+    const submitHandler = async (event) => {
+        event.preventDefault()
+        if (!password) {
+            // Don't submit, set alert
+            console.log('No pwd match')
+        }
+        else {
+            console.log('success')
+        }
+    }
+
     return (
-        <div>
-           Login 
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={submitHandler}>
+                <div className="form-group">
+                <input type="email" placeholder="Email Address" name="email" onChange={changeHandler}/>
+                <small className="form-text"
+                    >This site uses Gravatar so if you want a profile image, use a
+                    Gravatar email</small
+                >
+                </div>
+                <div className="form-group">
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    minLength="6"
+                    onChange={changeHandler}
+                />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/login">Register</Link>
+            </p>
+        </Fragment>
     )
 }
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react'
-import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export const Register = () => {
     const [formData, setFormData] = useState({
@@ -26,26 +26,7 @@ export const Register = () => {
             console.log('No pwd match')
         }
         else {
-            const newUser = {
-                name, email, password
-            }
-            try {
-                // NOTE: default axios headers are this, not required
-                const config = {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                }
-                //May not need to do this; seems like passing raw user works as well
-                const body = JSON.stringify(newUser)
-
-                // We can post this directly to our route b/c of our proxy
-                const res = await axios.post('/api/users', body, config)
-
-                console.log(res.data)
-            } catch (error) {
-                console.error(error.response.data)
-            }
+            console.log('success')
         }
     }
 
@@ -87,7 +68,7 @@ export const Register = () => {
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to="/login">Sign In</Link>
             </p>
         </Fragment>
     )
